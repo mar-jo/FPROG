7c9663c042d1a7c2b10ad08e7df0b2293d5c4a6b
mark <markx931@gmail.com>
2023-09-11 19:52:21 +0200

    Fix

--- A2/Example 1/pythagoras.cpp
+++ A2/Example 1/pythagoras.cpp
@@ -2,14 +2,12 @@
 #include <sstream>
 #include <tuple>
 
-using namespace std;
-
-const void print(const string& output)
+const void print(const std::string& output)
 {
-  cout << output;
+  std::cout << output;
 }
 
-const tuple<string, int> square(const int e)
+const std::tuple<std::string, int> square(const int e)
 {
 	double dummy = e;
 	double dummy2 = e;
@@ -27,21 +25,21 @@ const tuple<string, int> square(const int e)
   return {output.str(), e_value};
 }
 
-const tuple<string, double> squareroot(const double x)
+const std::tuple<std::string, double> squareroot(const double x)
 {				/* computes the square root of x */
 
   if (x == 0)
     return {"guess: 0", 0};
 
 /* the sqrt must be between xhi and xlo */
-  stringstream output;
+  std::stringstream output;
   double xhi = x;
   double xlo = 0;
   double guess = x / 2;
 
 /* We stop when guess*guess-x is very small */
 
-  while (abs (guess * guess - x) > 0.00001)
+  while (std::abs (guess * guess - x) > 0.00001)
   {
     if (guess * guess > x)
     {
@@ -56,39 +54,39 @@ const tuple<string, double> squareroot(const double x)
     guess = (xhi + xlo) / 2;
   }
 
-  output << "guess:" << guess << endl;;
+  output << "guess:" << guess << std::endl;;
   return {output.str(), guess};
 }
 
 
-const tuple<string, int> pythagoras(const int x, const int y)
+const std::tuple<std::string, int> pythagoras(const int x, const int y)
 {
-  stringstream output;
+  std::stringstream output;
 
   auto square_x = square(x);
   auto square_y = square(y);
 
-  output << get<0>(square_x);
-  output << get<0>(square_y);
+  output << std::get<0>(square_x);
+  output << std::get<0>(square_y);
 
-  auto z = squareroot (get<1>(square_x) + get<1>(square_y));
-  output << get<0>(z);
-  output << "euklid:" << get<1>(z) << endl;
+  auto z = squareroot (std::get<1>(square_x) + std::get<1>(square_y));
+  output << std::get<0>(z);
+  output << "euklid:" << std::get<1>(z) << std::endl;
 
-  return {output.str(), get<1>(z)};
+  return {output.str(), std::get<1>(z)};
 }
 
 int main (int argc, char *argv[])
 {
-  stringstream output;
-  string output_string;
+  std::stringstream output;
+  std::string output_string;
   int x = 3;
   int y = 4;
 
   auto pythagoras_z = pythagoras(x, y);
-  print(get<0>(pythagoras_z));
+  print(std::get<0>(pythagoras_z));
 
-  output << get<1>(pythagoras_z) << endl;
+  output << std::get<1>(pythagoras_z) << std::endl;
   output_string = output.str();
 
   print(output_string);
