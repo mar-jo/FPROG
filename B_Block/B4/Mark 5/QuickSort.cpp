b34ec32a9aa645ffd87ef877482371fee5e0dd05
mar-jo <if21b242@technikum-wien.at>
2023-10-02 17:24:43 +0200

    Optimized QuickSort & Added more Test Cases

--- B_Block/B4/Mark 5/QuickSort.cpp
+++ B_Block/B4/Mark 5/QuickSort.cpp
@@ -15,23 +15,19 @@ std::function<std::vector<int>(const std::vector<int>&)> quickSort = [](const st
 
     int pivot = data.front();
 
-    std::vector<int> less, equal, greater;
+    std::vector<int> less, greater;
 
-    std::accumulate(data.begin(), data.end(), std::make_tuple(&less, &equal, &greater), [&pivot](const auto& acc, const int val) 
+    std::accumulate(data.begin() + 1, data.end(), std::make_tuple(&less, &greater), [&pivot](const auto& acc, const int val) 
     {
-        auto& [less, equal, greater] = acc;
+        auto& [copy_less, copy_greater] = acc;
 
-        if (val < pivot)
+        if (val <= pivot)
         {
-            less->push_back(val);
-        }
-        else if (val == pivot) 
-        {
-            equal->push_back(val);
+            copy_less->push_back(val);
         }
         else
         {
-            greater->push_back(val);
+            copy_greater->push_back(val);
         }
 
         return acc;
@@ -40,7 +36,7 @@ std::function<std::vector<int>(const std::vector<int>&)> quickSort = [](const st
     auto sorted_less = quickSort(less);
     auto sorted_greater = quickSort(greater);
 
-    sorted_less.insert(sorted_less.end(), equal.begin(), equal.end());
+    sorted_less.push_back(pivot);
     sorted_less.insert(sorted_less.end(), sorted_greater.begin(), sorted_greater.end());
 
     return sorted_less;
@@ -48,7 +44,17 @@ std::function<std::vector<int>(const std::vector<int>&)> quickSort = [](const st
 
 TEST_CASE("quickSort")
 {
-    SUBCASE("Randomized Vector") 
+    SUBCASE("Randomized Vector - even") 
+    {
+        std::vector<int> inputData = {5, 2, 8, 1, 3, 7, 4, 6};
+
+        std::vector<int> sortedData = quickSort(inputData);
+        std::vector<int> expectedOutput = {1, 2, 3, 4, 5, 6, 7, 8};
+        
+        CHECK_EQ(expectedOutput, sortedData);
+    }
+
+    SUBCASE("Randomized Vector - uneven") 
     {
         std::vector<int> inputData = {5, 2, 8, 1, 9, 3, 7, 4, 6};
 
@@ -74,6 +80,26 @@ TEST_CASE("quickSort")
 
         std::vector<int> sortedData = quickSort(inputData);
         std::vector<int> expectedOutput = {1, 2, 3, 4, 5};
+
+        CHECK_EQ(expectedOutput, sortedData);
+    }
+
+    SUBCASE("Single value vector") 
+    {
+        std::vector<int> inputData = {1};
+
+        std::vector<int> sortedData = quickSort(inputData);
+        std::vector<int> expectedOutput = {1};
+
+        CHECK_EQ(expectedOutput, sortedData);
+    }
+
+    SUBCASE("Empty vector") 
+    {
+        std::vector<int> inputData = {};
+
+        std::vector<int> sortedData = quickSort(inputData);
+        std::vector<int> expectedOutput = {};
 
         CHECK_EQ(expectedOutput, sortedData);
     }
